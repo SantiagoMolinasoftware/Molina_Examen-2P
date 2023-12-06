e7b1ea0b631d4d3c5226bbfc56d598524285c6e1
Santiago Molina <santiago.molina.olivo@udla.edu.ec>
2023-12-06 15:46:29 -0500

    2do commit: definicion de variables y creacion del menu

--- main.c
+++ main.c
@@ -10,3 +10,51 @@ void GenerarNotasAleatorias(int calificaciones[][NumeroProgresos]);
 void MostrarNotasAlumnos(int calificaciones[][NumeroProgresos]);
 void CalcularPromedioGrupo(int calificaciones[][NumeroProgresos]);
 void EncontrarMejorEstudiante(int calificaciones[][NumeroProgresos]);
+
+int main() 
+{
+    srand(time(NULL));
+
+    int calificaciones[NumeroAlumnos][NumeroProgresos];
+
+    // Generar aleatoriamente las notas
+    GenerarNotasAleatorias(calificaciones);
+
+    int opcion;
+    do {
+        // Mostrar menú
+        printf("\nMENU:\n");
+        printf("1. Mostrar notas de todos los alumnos.\n");
+        printf("2. Mostrar nota promedio del grupo para cada progreso.\n");
+        printf("3. Mostrar al alumno con mayor promedio durante el semestre.\n");
+        printf("0. Salir.\n");
+        printf("Ingrese la opcion: ");
+        scanf("%d", &opcion);
+
+        switch (opcion) {
+            case 1:
+                // Mostrar notas de todos los alumnos
+                MostrarNotasAlumnos(calificaciones);
+                break;
+
+            case 2:
+                // Mostrar nota promedio del grupo para cada progreso
+                CalcularPromedioGrupo(calificaciones);
+                break;
+
+            case 3:
+                // Mostrar alumno con mayor promedio
+                EncontrarMejorEstudiante(calificaciones);
+                break;
+
+            case 0:
+                printf("Saliendo del programa.\n");
+                break;
+
+            default:
+                printf("Opcion no valida. Intente de nuevo.\n");
+        }
+    } while (opcion != 0);
+
+    return 0;
+}
